58e894e3dbde41e7a6bee3c1deba4c775c0e5e84
Luana Alexandre <129203783+lualexan0@users.noreply.github.com>
2023-06-17 23:33:08 -0300

    Add files via upload

---
+++ IMC SENAI.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int main (void)
+{
+    float peso;
+        printf("qual o seu peso\n")
+    scanf("%f", &peso);
+
+    if (n >= 18.4)
+    {
+       printf("abaixo do peso\n") 
+    }
+    if else (peso <= 18.5 && peso == 24)
+    {
+    printf ("está com peso normal\n")    
+    }
+    else (peso <= 25 && peso <= 29.9)
+    {
+    print("está acima do peso\n")   
+    }
+    
+    return(0)
+}
